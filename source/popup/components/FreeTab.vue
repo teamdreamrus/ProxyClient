93d62ef310af6581cc6a0e6be7a6012a7956bd3d
teamdreamrus <atazhanov.biz@gmail.com>
2020-04-17 14:31:25 +0700

    FreeTab: form and simple validation

--- source/popup/components/FreeTab.vue
+++ source/popup/components/FreeTab.vue
@@ -1,12 +1,76 @@
 <template>
     <div>
-        <h1>free tab</h1>
+        <b-form @submit.prevent="onSubmit" @reset="onReset">
+            <b-form-group
+                    id="Country"
+                    description=""
+                    label="Country"
+                    label-for="country"
+                    :invalid-feedback="invalidFeedbackCountry"
+                    :valid-feedback="validFeedbackCountry"
+                    :state="validationCountry"
+            >
+                <b-form-select :state="validationCountry" v-model="selected" :options="options"
+                               id="country"></b-form-select>
+            </b-form-group>
+            <button :disabled="!validationCountry" class="btn btn-success"
+                    type="submit">Connect
+            </button>
+        </b-form>
     </div>
 </template>
 
 <script>
   export default {
-    name: 'FreeTab'
+    name: 'FreeTab',
+    data() {
+      return {
+        selected: null,
+        options: [
+          {
+            value: null,
+            text: 'Please select some country'
+          },
+          {
+            value: 'RU',
+            text: 'Russia'
+          },
+          {
+            value: 'GB',
+            text: 'United Kingdom'
+          },
+          {
+            value: 'US',
+            text: 'USA'
+          },
+          {
+            value: 'NL',
+            text: 'Netherlands'
+          }
+        ]
+      };
+    },
+    methods: {
+      onSubmit() {
+        console.log('submit ', this.selected);
+      },
+      onReset() {
+
+      }
+    },
+    computed: {
+      validationCountry(){
+        return this.selected!=null;
+      },
+      validFeedbackCountry() {
+        if(this.validationCountry)
+          return 'A good choice';
+      },
+      invalidFeedbackCountry() {
+        if(!this.validationCountry)
+          return 'Must make a choice'
+      }
+    }
   };
 </script>
 
